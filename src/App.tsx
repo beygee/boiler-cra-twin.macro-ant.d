88f278878066eaca23522d2dfd8ac99840920e4d
Beygee <doug0476@naver.com>
2021-06-12 15:13:08 +0900

    feat: beatify app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import tw, { styled } from 'twin.macro'
+import { Result } from 'antd'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Section>
+      <Result status="success" title="Successfully installed" />
+    </Section>
+  )
 }
 
-export default App;
+const Section = styled.section`
+  ${tw`w-screen h-screen flex items-center justify-center`}
+`
+
+export default App
